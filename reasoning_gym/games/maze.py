c4b37fd1afcc06d86871b27e584158e0951f5e51
Andreas Koepf <andreas.koepf@aleph-alpha.com>
2025-01-24 23:47:52 +0100

    add question hint for maze dataset

--- reasoning_gym/games/maze.py
+++ reasoning_gym/games/maze.py
@@ -95,7 +95,8 @@ class MazeDataset(ProceduralDataset):
                     f"Navigate from '{self.start_char}' (start) to '{self.goal_char}' (goal):\n\n"
                     + self._maze_to_str(maze_grid)
                     + "\nLegend: "
-                    + f"'{self.wall_char}' = Wall, '{self.path_char}' = Path\n"
+                    + f"'{self.wall_char}' = Wall, '{self.path_char}' = Passage\n\n"
+                    + "What is the minimum number of steps to reach the goal?"
                 )
 
                 return {
