f7313d409c3905edb262df82230c8b487db0af78
Andreas Koepf <andreas.koepf@provisio.com>
2025-01-28 22:20:22 +0000

    use more realistic hparams for OpenRLHF example

--- examples/OpenRLHF/custom_reward_ppo.sh
+++ examples/OpenRLHF/custom_reward_ppo.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 args=(
    custom_reward.py
-   --pretrain meta-llama/Llama-3.2-1B-Instruct     # OpenRLHF/Llama-3-8b-sft-mixture
-   --save_path ./checkpoint/Llama-3.2-1b-lr        # ./checkpoint/llama-3-8b-rlhf
+   --pretrain meta-llama/Llama-3.2-3B-Instruct  # meta-llama/Llama-3.2-1B-Instruct
+   --save_path ./checkpoint/Llama-3.2-3B-rl     # ./checkpoint/Llama-3.2-1b-lr
    --save_steps -1
    --logging_steps 1
    --eval_steps -1
    --micro_train_batch_size 2
-   --train_batch_size 16
-   --micro_rollout_batch_size 4
-   --rollout_batch_size 64    # 1024
+   --train_batch_size 128
+   --micro_rollout_batch_size 16
+   --rollout_batch_size 1024
    --max_epochs 1
    --prompt_max_len 1024
    --generate_max_len 1024
