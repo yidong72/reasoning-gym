e26161713e83ab4c116de356fe0d99f6cbd6d0c6
Oliver <olivergestanley@gmail.com>
2025-02-20 22:04:01 +0000

    Answer scoring fixes to address edge cases

--- reasoning_gym/games/futoshiki.py
+++ reasoning_gym/games/futoshiki.py
@@ -637,7 +637,9 @@ class FutoshikiDataset(ProceduralDataset):
             row = 0
             num_matching = 0
             for ln in answer.split("\n"):
-                numbers = [int(c) for c in ln if c.isnumeric()]
+                if row >= len(solution):
+                    break
+                numbers = [int(c) for c in ln if c in "123456789"]
                 if len(numbers) != len(solution[0]):
                     continue  # ignore lines without numbers
                 for a, b in zip(solution[row], numbers):
