acb2d7eb53ff330d73671294d0038d9cd609395b
Andreas Koepf <andreas.koepf@provisio.com>
2025-02-26 20:54:07 +0100

    add llama-3.3-70b-instruct eval yaml files

---
+++ eval/scripts/run_llama-3.3-70-instruct_all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# run this script from the parent directory
+./eval.py --yaml algebra.yaml
+./eval.py --yaml algorithmic.yaml
+./eval.py --yaml arc.yaml
+./eval.py --yaml arithmetic.yaml
+./eval.py --yaml code.yaml
+./eval.py --yaml cognition.yaml
+./eval.py --yaml games.yaml
+./eval.py --yaml geometry.yaml
+./eval.py --yaml graphs.yaml
+./eval.py --yaml logic.yaml
