a6c90f40a14fae143cc2e432eb8a12833d4eec5b
Rich Jones <miserlou@gmail.com>
2025-02-27 13:44:33 +0100

    rm typo

--- reasoning_gym/algorithmic/graph_color.py
+++ reasoning_gym/algorithmic/graph_color.py
@@ -200,7 +200,7 @@ Vertices: {puzzle["vertices"]}
 Edges: {edges}
 Possible colors: {puzzle["color_options"]}
 
-Return your solution as a JSON map of vertices to colors. (For example: {{"0": 1, "1": 2, "2": 3}}. Remember )
+Return your solution as a JSON map of vertices to colors. (For example: {{"0": 1, "1": 2, "2": 3}}.)
 """
 
         return {
